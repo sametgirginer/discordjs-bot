26c1cf080b795b8a323fb2f74c7dd7730aa13d79
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2020-10-23 03:37:42 +0300

    ekleme yapıldı

--- functions/permission.js
+++ functions/permission.js
@@ -1,8 +1,15 @@
 module.exports = {
-    permCheck: function(message, perms) {
-        if(message.member.hasPermission(perms))
-            return true;
+    permCheck: function(message, perms, userID) {
+        if (userID === undefined) {
+            if(message.member.hasPermission(perms))
+                return true;
+
+            return false;
+        } else {
+            if (message.channel.permissionsFor(userID).has(perms))
+                return true;
             
-        return false;
-    }
+            return false;
+        }
+    },
 }
