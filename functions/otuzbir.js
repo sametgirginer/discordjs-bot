a762f1cbff7bd6960e038859bfc10995fe338d09
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2020-10-23 03:37:23 +0300

    konsolda gözüken hata giderildi

--- functions/otuzbir.js
+++ functions/otuzbir.js
@@ -1,43 +1,38 @@
+const { permCheck } = require('./permission');
 const fs = require('fs');
 const pathFile = `data/otuzbir.json`;
 
-const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
-
 module.exports = {
-    otuzbir: async function(message) {
-        let saatDurum = fs.readFileSync(pathFile);
-        saatDurum = JSON.parse(saatDurum);
+    otuzbir: async function(client, message) {
+        if (permCheck(message, 'SEND_MESSAGES', client.user.id)) {
+            let saatDurum = fs.readFileSync(pathFile);
+            saatDurum = JSON.parse(saatDurum);
 
-        let saat = new Date().getHours();
-        let dakika = new Date().getUTCMinutes();
+            let saat = new Date().getHours();
+            let dakika = new Date().getUTCMinutes();
 
-        saatDurum.forEach(s => {
+            saatDurum.forEach(s => {
 
-            if (s[saat] === false && dakika === 31) { 
-                message.channel.send('saate bakın KŞLSDAGKŞLSDKGŞALSDKGLŞAKSDG');
-                
-                s[saat] = true;
-                fs.open(pathFile, 'a', (err) => {
-                    if (err) throw err;
-                    fs.writeFileSync(pathFile, '', function(){ });
-                    sleep(10).then(() => {
+                if (s[saat] === false && dakika === 31) { 
+                    message.channel.send('saate bakın KŞLSDAGKŞLSDKGŞALSDKGLŞAKSDG');
+                    
+                    s[saat] = true;
+                    fs.open(pathFile, 'as+', (err) => {
+                        if (err) throw err;
+                        fs.writeFileSync(pathFile, '', function(){ });
                         fs.appendFileSync(pathFile, JSON.stringify(saatDurum, null, 4), { encoding: "utf8"});
                     });
-                });
-            } else if (dakika != 31) {
-                s[saat] = false;
+                } else if (dakika != 31) {
+                    s[saat] = false;
 
-                fs.open(pathFile, 'a', (err) => {
-                    if (err) throw err;
-                    fs.writeFileSync(pathFile, '', function(){ });
-                    sleep(10).then(() => {
+                    fs.open(pathFile, 'as+', (err) => {
+                        if (err) throw err;
+                        fs.writeFileSync(pathFile, '', function(){ });
                         fs.appendFileSync(pathFile, JSON.stringify(saatDurum, null, 4), { encoding: "utf8"});
                     });
-                });
-            }
-        });
+                }
+            });
+        }
     }
 }
 
