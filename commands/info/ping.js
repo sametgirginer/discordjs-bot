1c84585148f5532ba61c6afc4dbf791164a2cb8b
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2020-12-28 10:06:23 +0300

    pong

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -17,7 +17,9 @@ module.exports = {
 
         const pingEmbed = new MessageEmbed()
             .setColor('#65bff0')
-            .setDescription(`Ping değerleri alındı!\nGecikme: **${Math.floor(msg.createdAt - message.createdAt)}ms**`)
+            .setDescription('Ping değerleri alındı!')
+            .addField('Gecikme', `${Math.floor(msg.createdAt - message.createdAt)}ms`, true)
+            .addField('API Gecikmesi', `${Math.round(client.ws.ping)}ms`, true)
 
         msg.edit(pingEmbed);
     }
