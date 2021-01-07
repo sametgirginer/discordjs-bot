8be97303a093746f99c000b3ea70fa92425eefb6
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2021-01-07 03:57:04 +0300

    artstation arama komutu eklendi

--- commands/data/artstation.js
+++ commands/data/artstation.js
@@ -1,66 +1,88 @@
 const { MessageEmbed } = require('discord.js');
 const { infoMsg } = require('../../functions/message.js');
 const request = require("request");
-const kategoriler = '`kurgu, 3d, komik, konsept, mimari, karakter, yaratık, manzara, oyun, çizim, mekanik, bilim, hayal, silah`';
+const { query } = require('express');
+const kategoriler = '`fantasy, archviz, comicart, konsept, mimari, karakter, yaratık, manzara, oyun, ill, mecha, scfi, sürreal, silah`';
 
 module.exports = {
     name: 'art',
     category: 'data',
-    description: 'artstation.com sitesinden veri çeker.',
+    description: `Random veri için ${process.env.prefix}art\n\nKategoriden random veri için: ${process.env.prefix}art kategori [${kategoriler}]\n\nArama yapmak için: ${process.env.prefix}art [xxx]`,
 	prefix: true,
     owner: false,
-    onlykeyubu: false,
 	permissions: ['VIEW_CHANNEL'],
-    run: async (client, message, args) => {
-		let randomUrl = 'https://www.artstation.com/random_project.json?&category=';
+    run: async (client, message, args) => {	
+		let apiUrl = "https://www.artstation.com/random_project.json?&category=";
+		let query = "";
 
-		if (args.length) {
+		if (args[0] === "kategori" || args[0] === "category") {
 			let kategori;
 
-			if (args[0] == 'fantasy' || args[0] == 'fantazi' || args[0] == 'kurgu' || args[0] == 'hayal') kategori = 'fantasy';
-			if (args[0] == 'archviz' || args[0] == '3d') kategori = 'archviz';
-			if (args[0] == 'comic' || args[0] == 'komik') kategori = 'comic_art';
-			if (args[0] == 'concept' || args[0] == 'konsept') kategori = 'concept_art';
-			if (args[0] == 'architecture' || args[0] == 'mimari' || args[0] == 'yapı' || args[0] == 'inşaat') kategori = 'architecture';
-			if (args[0] == 'character' || args[0] == 'char' || args[0] == 'karakter' || args[0] == 'reel') kategori = 'characters';
-			if (args[0] == 'creature' || args[0] == 'yaratık' || args[0] == 'varlık') kategori = 'creatures';
-			if (args[0] == 'environment' || args[0] == 'manzara' || args[0] == 'çevre' || args[0] == 'cevre' || args[0] == 'ortam') kategori = 'environments';
-			if (args[0] == 'game' || args[0] == 'oyun') kategori = 'game_art';
-			if (args[0] == 'illustration' || args[0] == 'ill' || args[0] == 'çizim') kategori = 'illustration';
-			if (args[0] == 'mecha' || args[0] == 'meka' || args[0] == 'mekanik') kategori = 'mecha';
-			if (args[0] == 'science' || args[0] == 'bilim' || args[0] == 'scfi') kategori = 'science_fiction';
-			if (args[0] == 'surreal' || args[0] == 'sürreal' || args[0] == 'hayal' || args[0] == 'hayali') kategori = 'surreal';
-			if (args[0] == 'weapon' || args[0] == 'silah') kategori = 'weapons';
+			if (args[1] == 'fantasy') kategori = 'fantasy';
+			if (args[1] == 'archviz') kategori = 'archviz';
+			if (args[1] == 'comicart') kategori = 'comic_art';
+			if (args[1] == 'concept' || args[1] == 'konsept') kategori = 'concept_art';
+			if (args[1] == 'architecture' || args[1] == 'mimari' || args[1] == 'yapı') kategori = 'architecture';
+			if (args[1] == 'character' || args[1] == 'char' || args[1] == 'karakter') kategori = 'character';
+			if (args[1] == 'creature' || args[1] == 'yaratık') kategori = 'creatures';
+			if (args[1] == 'environment' || args[1] == 'manzara' || args[1] == 'çevre') kategori = 'environments';
+			if (args[1] == 'game' || args[1] == 'oyun') kategori = 'game_art';
+			if (args[1] == 'illustration' || args[1] == 'ill' || args[1] == 'çizim') kategori = 'illustration';
+			if (args[1] == 'mecha') kategori = 'mecha';
+			if (args[1] == 'science' || args[1] == 'scfi') kategori = 'science_fiction';
+			if (args[1] == 'surreal' || args[1] == 'sürreal') kategori = 'surreal';
+			if (args[1] == 'weapon' || args[1] == 'silah') kategori = 'weapons';
 
-			if (kategori == '' || kategori == null) return infoMsg(message, '65bff0', `<@${message.author.id}>, geçerli bir kategori ismi girmelisin.\nKategoriler: ${kategoriler}`, true, 15000);
-			else randomUrl += kategori;
+			if (kategori == '' || kategori == null) return infoMsg(message, '65bff0', `<@${message.author.id}>, geçerli bir kategori ismi girmelisin.\n\nKategoriler: ${kategoriler}`, true);
+			else apiUrl = `https://www.artstation.com/random_project.json?&category=${kategori}`;
+		} else if (args.length) {
+			
+			args.forEach(arg => {
+				query += `${arg}${(args.length > 1) ? " " : ""}`;
+			});
+			apiUrl = `https://www.artstation.com/api/v2/search/projects.json?query=${query}&page=1&per_page=50`;
 		}
 
 		request({
-			url: randomUrl,
+			url: apiUrl,
 			json: true,
 			jsonReplacer: true
 		}, function(err, response, body) {
 			if (!err && response.statusCode === 200) {
 				let out = JSON.parse(JSON.stringify(body));
-				let title = out.title.toString();
-				let usrUrl = 'Profil: https://artstation.com/' + out.user.username.toString();
-				let viewCount = out.views_count.toString();
-				let likeCount = out.likes_count.toString();
-				let image = out.cover.large_image_url.toString();
 
-				const artEmbed = new MessageEmbed()
-				    .setColor('#' + (Math.random()*0xFFFFFF<<0).toString(16))
-				    .setDescription(usrUrl)
-				    .setAuthor(title, image.embedAvatar)
-				    .addField('Görüntülenme', viewCount, true)
-				    .addField('Beğeni', likeCount, true)
-				    .setImage(image)
-				    .setTimestamp()
-				    .setFooter(message.author.username + '#' + message.author.discriminator + ' tarafından kullanıldı.');
+				if (out.id != undefined) {
+					const artEmbed = new MessageEmbed()
+						.setColor('RANDOM')
+						.setDescription(`Tarayıcıda görüntülemek için [buraya](${out.permalink}) tıklayın.`)
+						.setAuthor(out.title, out.user.medium_avatar_url)
+						.addField('Görüntülenme', out.views_count, true)
+						.addField('Beğeni', out.likes_count, true)
+						.addField('Yorum', out.comments_count, true)
+						.setImage(out.cover.large_image_url)
+						.setTimestamp()
+						.setFooter(message.author.username + '#' + message.author.discriminator);
 
-                message.delete({ timeout: 0, reason: 'Otomatik bot işlemi.' });
-				return message.channel.send(artEmbed);
+					message.delete({ timeout: 0, reason: 'Otomatik bot işlemi.' });
+					return message.channel.send(artEmbed);
+				} else {
+					let rn = Math.ceil(Math.random() * 50);
+					//console.log(out.total_count);
+					/*out.data.forEach(q => {
+						console.log(q);
+					});*/
+					
+					const artEmbed = new MessageEmbed()
+						.setColor('RANDOM')
+						.setDescription(`**${query}** araması ile ilgili veri getirildi.\nTarayıcıda görüntülemek için [buraya](${out.data[rn].url}) tıklayın.\n\nArama fonksiyonu yapım aşamasındadır.`)
+						.setAuthor(out.data[rn].title, out.data[rn].smaller_square_cover_url)
+						.setImage(out.data[rn].smaller_square_cover_url)
+						.setTimestamp()
+						.setFooter(message.author.username + '#' + message.author.discriminator);
+
+					message.delete({ timeout: 0, reason: 'Otomatik bot işlemi.' });
+					return message.channel.send(artEmbed);
+				}
 			} else {
                 message.delete({ timeout: 0, reason: 'Otomatik bot işlemi.' });
 				return infoMsg(message, 'B20000', `<@${message.author.id}>, veri çekme işleminde bir hata oluştu.`, true, 10000)
