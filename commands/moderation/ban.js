bcfdb133d3cf5c6b53d267e12f5e7a714acef45b
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2021-05-31 16:38:18 +0300

    "Missing Permissions" fix

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -12,25 +12,21 @@ module.exports = {
     run: async (client, message, args) => {
         if (!message.mentions.members.size) return infoMsg(message, 'B20000', `<@${message.author.id}>, herhangi bir kişiyi etiketlemedin.`, true, 500);
 
-        if (message.mentions.members.size === 1) {
-            if (args[1]) {
-                message.mentions.members.forEach(member => {
-                    if (member.hasPermission(['BAN_MEMBERS']) || member.hasPermission(['KICK_MEMBERS'])) return infoMsg(message, 'B20000', `<@${message.author.id}>, bu discord kullanıcısını yasaklayamazsın.`, true, 5000);
+        message.mentions.members.forEach(member => {
+            if (!member.bannable || member.hasPermission(['BAN_MEMBERS']) || member.hasPermission(['KICK_MEMBERS'])) return infoMsg(message, 'B20000', `<@${message.author.id}>, bu discord kullanıcısını yasaklayamazsın.`, true, 5000);
 
-                    let reason = '';
-                    args.forEach(arg => {
-                        if (`<@!${member.id}>` != arg) reason += `${arg} `;
-                    });
-
-                    member.ban({ days: 7, reason: `Sebep: ${reason}` });
-                    return infoMsg(message, '83eb34', `<@${member.id}>, discord sunucusundan yasaklandı.\nSunucudan yasaklanma sebebi: **${reason}**\nSunucudan yasaklayan yetkili: <@${message.author.id}>`);
+            if(args[1]) {
+                let reason = '';
+                args.forEach(arg => {
+                    if (`<@!${member.id}>` != arg) reason += `${arg} `;
                 });
+
+                member.ban({ days: 7, reason: `Sebep: ${reason}` });
+                return infoMsg(message, '83eb34', `<@${member.id}>, discord sunucusundan yasaklandı.\nSunucudan yasaklanma sebebi: **${reason}**\nSunucudan yasaklayan yetkili: <@${message.author.id}>`);
             } else {
-                message.mentions.members.forEach(member => {
-                    member.ban({ days: 7, reason: 'Sebep girilmedi.' });
-                    return infoMsg(message, '83eb34', `<@${member.id}>, discord sunucusundan yasaklandı.\nSunucudan yasaklayan yetkili: <@${message.author.id}>`);
-                });
+                member.ban({ days: 7, reason: 'Sebep girilmedi.' });
+                return infoMsg(message, '83eb34', `<@${member.id}>, discord sunucusundan yasaklandı.\nSunucudan yasaklayan yetkili: <@${message.author.id}>`);
             }
-        }
+        });
     }
 }
--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -12,25 +12,21 @@ module.exports = {
     run: async (client, message, args) => {
         if (!message.mentions.members.size) return infoMsg(message, 'B20000', `<@${message.author.id}>, herhangi bir kişiyi etiketlemedin.`, true, 5000);
 
-        if (message.mentions.members.size === 1) {
+        message.mentions.members.forEach(member => {
+            if (!member.kickable || member.hasPermission(['BAN_MEMBERS']) || member.hasPermission(['KICK_MEMBERS'])) return infoMsg(message, 'B20000', `<@${message.author.id}>, bu discord kullanıcısını atamazsın.`, true, 5000);
+
             if (args[1]) {
-                message.mentions.members.forEach(member => {
-                    if (member.hasPermission(['BAN_MEMBERS']) || member.hasPermission(['KICK_MEMBERS'])) return infoMsg(message, 'B20000', `<@${message.author.id}>, bu discord kullanıcısını atamazsın.`, true, 5000);
-
-                    let reason = '';
-                    args.forEach(arg => {
-                        if (`<@!${member.id}>` != arg) reason += `${arg} `;
-                    });
-
-                    member.kick(`Sebep: ${reason}`);
-                    return infoMsg(message, '83eb34', `<@${member.id}>, discord sunucusundan atıldı.\nSunucudan atılma sebebi: **${reason}**\nSunucudan atan yetkili: <@${message.author.id}>`);
+                let reason = '';
+                args.forEach(arg => {
+                    if (`<@!${member.id}>` != arg) reason += `${arg} `;
                 });
+
+                member.kick(`Sebep: ${reason}`);
+                return infoMsg(message, '83eb34', `<@${member.id}>, discord sunucusundan atıldı.\nSunucudan atılma sebebi: **${reason}**\nSunucudan atan yetkili: <@${message.author.id}>`);
             } else {
-                message.mentions.members.forEach(member => {
-                    member.kick('Sebep girilmedi.');
-                    return infoMsg(message, '83eb34', `<@${member.id}>, discord sunucusundan atıldı.\nSunucudan atan yetkili: <@${message.author.id}>`);
-                });
+                member.kick('Sebep girilmedi.');
+                return infoMsg(message, '83eb34', `<@${member.id}>, discord sunucusundan atıldı.\nSunucudan atan yetkili: <@${message.author.id}>`);
             }
-        }
+        });
     }
 }
