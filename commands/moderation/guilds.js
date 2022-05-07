f97909fee8824a37999935bbe2787aecd68880b6
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2022-05-07 05:15:41 +0300

    create invite for guilds command

--- commands/moderation/guilds.js
+++ commands/moderation/guilds.js
@@ -11,11 +11,26 @@ module.exports = {
     permissions: ['ADMINISTRATOR'],
     run: async (client, message, args) => {
         let guilds = "";
+        let aend = 0;
 
-        client.guilds.cache.forEach(guild => {
-            guilds += guild.name + "\n";
+        client.guilds.cache.forEach(async guild => {
+            if (!guild.members.cache.get(client.user.id).permissions.has('CREATE_INSTANT_INVITE')) return;
+
+            let channel = await guild.channels.cache.filter(c=> c.permissionsFor(guild.me).has('CREATE_INSTANT_INVITE')).random();
+
+            if (channel) {
+                try {
+                    let invite = await guild.invites.create(channel.id);
+                    guilds += `${guild.name} - ${await invite.url}\n`;
+                } catch (error) {
+                    return;
+                }
+            } else {
+                guilds += `${guild.name}\n`;
+            }
+
+            aend++;
+            if (aend === client.guilds.cache.size) return infoMsg(message, 'RANDOM', `${guilds}`);
         });
-        
-        infoMsg(message, 'RANDOM', `${guilds}`);
     }
 }
