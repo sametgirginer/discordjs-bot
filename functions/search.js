fa5af3bda4e520b8e2d58fef7ada3752a88d5236
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2022-05-08 04:52:50 +0300

    get member by id

--- functions/search.js
+++ functions/search.js
@@ -1,4 +1,10 @@
 module.exports = {
+    member: async function(message, id) {
+        if (!id) id = message.author.id;
+        let member = message.guild.members.cache.get(id);
+        return (member) ? member : false;
+    },
+
     user: async function(client, id, message, name) {
         if (id != null) return client.users.fetch(id);
         else if (message != null) {
