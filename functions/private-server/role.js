174e63a2247e5f4004976004bd651061dc2497d0
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2022-05-16 21:24:40 +0300

    role names changed

--- functions/private-server/role.js
+++ functions/private-server/role.js
@@ -2,81 +2,81 @@ module.exports = {
     levelup: async function(guild, member, level, messageOrGuild) {
         if (guild != process.env.supportserver) return;
 
-        let Blackmailer,Consigliere,Consort,Disguiser,Framer,Janitor,Arsonist,Mafioso,Hypnotist,Godfather,mu = "";
+        let r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,mu = "";
 
         if (messageOrGuild.author != undefined) {
-            Blackmailer = messageOrGuild.guild.roles.cache.find(r => r.name === "Blackmailer");
-            Consigliere = messageOrGuild.guild.roles.cache.find(r => r.name === "Consigliere");
-            Consort = messageOrGuild.guild.roles.cache.find(r => r.name === "Consort");
-            Disguiser = messageOrGuild.guild.roles.cache.find(r => r.name === "Disguiser");
-            Framer = messageOrGuild.guild.roles.cache.find(r => r.name === "Framer");
-            Janitor = messageOrGuild.guild.roles.cache.find(r => r.name === "Janitor");
-            Arsonist = messageOrGuild.guild.roles.cache.find(r => r.name === "Arsonist");
-            Mafioso = messageOrGuild.guild.roles.cache.find(r => r.name === "Mafioso");
-            Hypnotist = messageOrGuild.guild.roles.cache.find(r => r.name === "Hypnotist");
-            Godfather = messageOrGuild.guild.roles.cache.find(r => r.name === "Godfather");
+            r1 = messageOrGuild.guild.roles.cache.find(r => r.name === "Ucube");
+            r2 = messageOrGuild.guild.roles.cache.find(r => r.name === "Homo Erectus");
+            r3 = messageOrGuild.guild.roles.cache.find(r => r.name === "Canavar");
+            r4 = messageOrGuild.guild.roles.cache.find(r => r.name === "Yaratık");
+            r5 = messageOrGuild.guild.roles.cache.find(r => r.name === "Ork");
+            r6 = messageOrGuild.guild.roles.cache.find(r => r.name === "Troll");
+            r7 = messageOrGuild.guild.roles.cache.find(r => r.name === "Goblin");
+            r8 = messageOrGuild.guild.roles.cache.find(r => r.name === "Gollum");
+            r9 = messageOrGuild.guild.roles.cache.find(r => r.name === "Gulyabani");
+            r10 = messageOrGuild.guild.roles.cache.find(r => r.name === "Gardiyan Ucube");
 
             mu = messageOrGuild.guild.members.cache.find(u => u.id === member);
         } else if (messageOrGuild.icon) {
-            Blackmailer = messageOrGuild.roles.cache.find(r => r.name === "Blackmailer");
-            Consigliere = messageOrGuild.roles.cache.find(r => r.name === "Consigliere");
-            Consort = messageOrGuild.roles.cache.find(r => r.name === "Consort");
-            Disguiser = messageOrGuild.roles.cache.find(r => r.name === "Disguiser");
-            Framer = messageOrGuild.roles.cache.find(r => r.name === "Framer");
-            Janitor = messageOrGuild.roles.cache.find(r => r.name === "Janitor");
-            Arsonist = messageOrGuild.roles.cache.find(r => r.name === "Arsonist");
-            Mafioso = messageOrGuild.roles.cache.find(r => r.name === "Mafioso");
-            Hypnotist = messageOrGuild.roles.cache.find(r => r.name === "Hypnotist");
-            Godfather = messageOrGuild.roles.cache.find(r => r.name === "Godfather");
+            r1 = messageOrGuild.roles.cache.find(r => r.name === "Ucube");
+            r2 = messageOrGuild.roles.cache.find(r => r.name === "Homo Erectus");
+            r3 = messageOrGuild.roles.cache.find(r => r.name === "Canavar");
+            r4 = messageOrGuild.roles.cache.find(r => r.name === "Yaratık");
+            r5 = messageOrGuild.roles.cache.find(r => r.name === "Ork");
+            r6 = messageOrGuild.roles.cache.find(r => r.name === "Troll");
+            r7 = messageOrGuild.roles.cache.find(r => r.name === "Goblin");
+            r8 = messageOrGuild.roles.cache.find(r => r.name === "Gollum");
+            r9 = messageOrGuild.roles.cache.find(r => r.name === "Gulyabani");
+            r10 = messageOrGuild.roles.cache.find(r => r.name === "Gardiyan Ucube");
 
             mu = messageOrGuild.members.cache.find(u => u.id === member);
         }
 
-        if (level >= 5 && level < 10) return mu.roles.add(Blackmailer, `LevelUP. level: ${level}`);
+        if (level >= 5 && level < 10) return mu.roles.add(r1, `LevelUP. level: ${level}`);
 
         if (level >= 10 && level < 15) {
-            mu.roles.remove(Blackmailer, `LevelUP. level: ${level}`);
-            mu.roles.add(Consigliere, `LevelUP. level: ${level}`);
+            mu.roles.remove(r1, `LevelUP. level: ${level}`);
+            mu.roles.add(r2, `LevelUP. level: ${level}`);
         }
 
         if (level >= 15 && level < 20) {
-            mu.roles.remove(Consigliere, `LevelUP. level: ${level}`);
-            mu.roles.add(Consort, `LevelUP. level: ${level}`);
+            mu.roles.remove(r2, `LevelUP. level: ${level}`);
+            mu.roles.add(r3, `LevelUP. level: ${level}`);
         }
 
         if (level >= 20 && level < 25) {
-            mu.roles.remove(Consort, `LevelUP. level: ${level}`);
-            mu.roles.add(Disguiser, `LevelUP. level: ${level}`);
+            mu.roles.remove(r3, `LevelUP. level: ${level}`);
+            mu.roles.add(r4, `LevelUP. level: ${level}`);
         }
 
         if (level >= 25 && level < 30) {
-            mu.roles.remove(Disguiser, `LevelUP. level: ${level}`);
-            mu.roles.add(Framer, `LevelUP. level: ${level}`);
+            mu.roles.remove(r4, `LevelUP. level: ${level}`);
+            mu.roles.add(r5, `LevelUP. level: ${level}`);
         }
 
         if (level >= 30 && level < 35) {
-            mu.roles.remove(Framer, `LevelUP. level: ${level}`);
-            mu.roles.add(Janitor, `LevelUP. level: ${level}`);
+            mu.roles.remove(r5, `LevelUP. level: ${level}`);
+            mu.roles.add(r6, `LevelUP. level: ${level}`);
         }
 
         if (level >= 35 && level < 40) {
-            mu.roles.remove(Janitor, `LevelUP. level: ${level}`);
-            mu.roles.add(Arsonist, `LevelUP. level: ${level}`);
+            mu.roles.remove(r6, `LevelUP. level: ${level}`);
+            mu.roles.add(r7, `LevelUP. level: ${level}`);
         }
 
         if (level >= 40 && level < 45) {
-            mu.roles.remove(Arsonist, `LevelUP. level: ${level}`);
-            mu.roles.add(Mafioso, `LevelUP. level: ${level}`);
+            mu.roles.remove(r7, `LevelUP. level: ${level}`);
+            mu.roles.add(r8, `LevelUP. level: ${level}`);
         }
 
         if (level >= 45 && level < 50) {
-            mu.roles.remove(Mafioso, `LevelUP. level: ${level}`);
-            mu.roles.add(Hypnotist, `LevelUP. level: ${level}`);
+            mu.roles.remove(r8, `LevelUP. level: ${level}`);
+            mu.roles.add(r9, `LevelUP. level: ${level}`);
         }
 
         if (level >= 50) {
-            mu.roles.remove(Hypnotist, `LevelUP. level: ${level}`);
-            mu.roles.add(Godfather, `LevelUP. level: ${level}`);
+            mu.roles.remove(r9, `LevelUP. level: ${level}`);
+            mu.roles.add(r10, `LevelUP. level: ${level}`);
         }
     }
 }
