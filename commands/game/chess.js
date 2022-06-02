cb0c8d4372f5997e998314d43c40eccdbe98070d
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2022-06-02 19:03:37 +0300

    language updated: chess/poker/youtube

--- commands/game/chess.js
+++ commands/game/chess.js
@@ -1,16 +1,17 @@
 const { infoMsg } = require('../../functions/message');
+const { buildText } = require('../../functions/language');
 const { DiscordTogether } = require('discord-together');
 
 module.exports = {
     name: 'chess',
     category: 'game',
-    description: 'Satranç oyunu başlatır.',
+    description: 'chess_desc',
     prefix: true,
 	owner: false,
 	supportserver: false,
 	permissions: ['VIEW_CHANNEL'],
     run: async (client, message, args) => {
-        if (!message.member.voice.channel) return infoMsg(message, 'B5200', `Komutu kullanmak için ses kanalına giriş yapmalısın.`, true);
+        if (!message.member.voice.channel) return infoMsg(message, 'B5200', await buildText("must_login_voice", client, { guild: message.guild.id }), true);
 
         client.discordTogether = new DiscordTogether(client);
         client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'chess').then(async invite => {
