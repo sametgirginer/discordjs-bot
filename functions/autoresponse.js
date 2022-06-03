db1618407fb8cb3021044e42115aab59d539e768
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2022-06-03 21:03:34 +0300

    language updated: autoresponse

--- functions/autoresponse.js
+++ functions/autoresponse.js
@@ -1,25 +1,27 @@
 const fs = require('fs');
-const { infoMsg } = require('./message')
-var cevaplar = fs.readFileSync('data/oto-cevap.json');
-cevaplar = JSON.parse(cevaplar);
+var data = fs.readFileSync('data/answers.json');
+data = JSON.parse(data);
 
 module.exports = {
     autoResponse: async function(message) {
-        let icerik = message.content.toLowerCase();
+        let content = message.content.toLowerCase();
 
-        cevaplar.forEach(c => {
-            if (Array.isArray(c.mesaj)) {
-                c.mesaj.forEach(m => {
-                    cevap = c.cevap.replace('@user', `@${message.author.id}`);
-                    if (c.ara === true) { 
-                        if (icerik.search(m) >= 0) return message.channel.send({ content: cevap });
-                    } else if (m === icerik) return message.channel.send({ content: cevap });
+        data.forEach(c => {
+            if (Array.isArray(c.content)) {
+                c.content.forEach(m => {
+                    m = m.toLowerCase();
+
+                    answer = c.answer.replace('@user', `@${message.author.id}`);
+                    if (c.search === true) { 
+                        if (content.search(m) >= 0) return message.channel.send({ content: answer });
+                    } else if (m === content) return message.channel.send({ content: answer });
                 });
             } else {
-                cevap = c.cevap.replace('@user', `@${message.author.id}`);
-                if (c.ara === true) {
-                    if (icerik.search(c.mesaj) >= 0) return message.channel.send({ content: cevap });
-                } else if (c.mesaj === icerik) return message.channel.send({ content: cevap });
+                c.content = c.content.toLowerCase();
+                answer = c.answer.replace('@user', `@${message.author.id}`);
+                if (c.search === true) {
+                    if (content.search(c.content) >= 0) return message.channel.send({ content: answer });
+                } else if (c.content === content) return message.channel.send({ content: answer });
             }
         });
     }
