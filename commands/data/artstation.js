13dfcff14286df6c717b1a8729a717a7aaf14125
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2022-06-20 19:24:48 +0300

    updated art cmd for interactionCreate

--- commands/data/artstation.js
+++ commands/data/artstation.js
@@ -1,35 +1,16 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-const { infoMsg } = require('../../functions/message');
 const { buildText } = require('../../functions/language');
 const request = require("request");
-const categories = ['fantasy', 'archviz', 'comic_art', 'concept_art', 'architecture', 'character', 'creatures', 'environments', 'game_art', 'illustration', 'mecha', 'science_fiction', 'surreal', 'weapons']
 
 module.exports = {
-    name: 'art',
-    category: 'data',
-    description: `artstation_desc`,
-	prefix: true,
-    owner: false,
-	permissions: ['VIEW_CHANNEL'],
-    run: async (client, message, args) => {	
+    run: async (client, interaction) => {
 		let apiUrl = "https://www.artstation.com/random_project.json?&category=";
-		let query = "";
+		let category = interaction.options.getString('category');
+		let query = interaction.options.getString('search');
 
-		if (args[0] === "kategori" || args[0] === "category") {
-			let category = null;
-
-			categories.forEach(c => {
-				if (c == args[1]) category = c;
-			});
-
-			if (category == null) return infoMsg(message, '65bff0', await buildText("artstation_select_category", client, { guild: message.guild.id, message: message }), true);
-			else apiUrl = `https://www.artstation.com/random_project.json?&category=${category}`;
-		} else if (args.length) {
-			args.forEach(arg => {
-				query += `${arg}${(args.length > 1) ? " " : ""}`;
-			});
-			apiUrl = `https://www.artstation.com/api/v2/search/projects.json?query="${query}"&page=1&per_page=75`;
-		}
+		if (category) apiUrl = `https://www.artstation.com/random_project.json?&category=${category}`;
+		else if (query) apiUrl = `https://www.artstation.com/api/v2/search/projects.json?query="${query}"&page=1&per_page=75&sorting=relevance`;
 
 		request({
 			url: apiUrl,
@@ -42,22 +23,21 @@ module.exports = {
 				if (out.id != undefined) {
 					const artEmbed = new MessageEmbed()
 						.setColor('RANDOM')
-						.setDescription(await buildText("artstation_click_here", client, { guild: message.guild.id, variables: [out.permalink] }))
+						.setDescription(await buildText("artstation_click_here", client, { guild: interaction.guildId, variables: [out.permalink] }))
 						.setAuthor({ name: out.title, url: out.user.medium_avatar_url })
 						.addFields([
-							{ name: await buildText("artstation_views", client, { guild: message.guild.id }), value: (out.views_count).toString(), inline: true },
-							{ name: await buildText("artstation_likes", client, { guild: message.guild.id }), value: (out.likes_count).toString(), inline: true },
-							{ name: await buildText("artstation_comments", client, { guild: message.guild.id }), value: (out.comments_count).toString(), inline: true }
+							{ name: await buildText("artstation_views", client, { guild: interaction.guildId }), value: (out.views_count).toString(), inline: true },
+							{ name: await buildText("artstation_likes", client, { guild: interaction.guildId }), value: (out.likes_count).toString(), inline: true },
+							{ name: await buildText("artstation_comments", client, { guild: interaction.guildId }), value: (out.comments_count).toString(), inline: true }
 						])
 						.setImage(out.cover.large_image_url)
 						.setTimestamp()
-						.setFooter({ text: `${message.author.username}#${message.author.discriminator}` });
+						.setFooter({ text: `${interaction.user.username}#${interaction.user.discriminator}` });
 
-					message.delete();
-					return message.channel.send({ embeds: [artEmbed] });
+					return interaction.reply({ embeds: [artEmbed] });
 				} else {
 					let page = Math.floor(Math.random() * (((out.total_count / 75) === 0) ? 1 : Math.floor(out.total_count / 75)) + 1);
-					apiUrl = `https://www.artstation.com/api/v2/search/projects.json?query="${query}"&page=${page}&per_page=75`;
+					apiUrl = `https://www.artstation.com/api/v2/search/projects.json?query="${query}"&page=${page}&per_page=75&sorting=relevance`;
 
 					request({
 						url: apiUrl,
@@ -68,29 +48,57 @@ module.exports = {
 							out = JSON.parse(JSON.stringify(await body));
 
 							let rn = Math.floor((Math.random() * 1) * Math.floor((out.total_count < 75) ? out.total_count-1 : 74));
-							if (out.total_count === 0) return infoMsg(message, 'FFE26A', await buildText("artstation_not_found", client, { guild: message.guild.id, message: message, variables: [query] }), true, 10000);
+							if (out.total_count === 0) return interaction.reply({ content: await buildText("artstation_not_found", client, { guild: interaction.guildId, variables: [query] }), ephemeral: true })
 		
+							let channel = client.guilds.cache.get(interaction.guildId).channels.cache.get(interaction.channelId);
 							if (out.data[rn].hide_as_adult === true)
-								if (message.channel.nsfw === false || message.channel.nsfw === undefined)
-									return infoMsg(message, 'FFE26A', await buildText("artstation_nsfw_content", client, { guild: message.guild.id, message: message }), true, 10000);
+								if (channel.nsfw === false) return interaction.reply({ content: await buildText("artstation_nsfw_content", client, { guild: interaction.guildId }), ephemeral: true })
 						
 							out.total_count = (out.total_count-1 === 0) ? 1 : out.total_count - 1;
 							const artEmbed = new MessageEmbed()
 								.setColor('RANDOM')
-								.setDescription(await buildText("artstation_search_found", client, { guild: message.guild.id, variables: [query, out.total_count, out.data[rn].url] }))
+								.setDescription(await buildText("artstation_search_found", client, { guild: interaction.guildId, variables: [query, out.total_count, out.data[rn].url] }))
 								.setAuthor({ name: out.data[rn].title, url: out.data[rn].smaller_square_cover_url })
 								.setImage(out.data[rn].smaller_square_cover_url)
 								.setTimestamp()
-								.setFooter({ text: message.author.username + '#' + message.author.discriminator });
+								.setFooter({ text: interaction.user.username + '#' + interaction.user.discriminator });
 		
-							message.delete();
-							return message.channel.send({ embeds: [artEmbed] });
+							return interaction.reply({ embeds: [artEmbed] });
 						}
 					});
 				}
 			} else {
-				return infoMsg(message, 'B20000', await buildText("artstation_data_error", client, { guild: message.guild.id, message: message }), true, 10000)
+				return interaction.reply({ content: await buildText("artstation_data_error", client, { guild: interaction.guildId }), ephemeral: true })
 			}
 		});
-    }
+    },
+
+	data: new SlashCommandBuilder()
+		.setName('art')
+		.setDescription('It pulls media data from artstation.com site.')
+		.setDMPermission(true)
+		.addStringOption(option =>
+			option.setName('search')
+				.setDescription('You can search for any media you want.')
+				.setRequired(false)
+		)
+		.addStringOption(option =>
+			option.setName('category')
+				.setDescription('You can select a category and bring media.')
+				.setRequired(false)
+				.addChoices(
+					{ name: 'Fantasy', value: 'fantasy' },
+					{ name: 'Architectural Visualization', value: 'archviz' },
+					{ name: 'Comic Art', value: 'comic_art' },
+					{ name: 'Concept Art', value: 'concept_art' },
+					{ name: 'Architectural Concepts', value: 'architecture' },
+					{ name: 'Character Design', value: 'character' },
+					{ name: 'Creatures', value: 'creatures' },
+					{ name: 'Environmental Concept Art & Design', value: 'environments' },
+					{ name: 'Game Art', value: 'game_art' },
+					{ name: 'Illustration', value: 'illustration' },
+					{ name: 'Science Fiction', value: 'science_fiction' },
+					{ name: 'Weapons', value: 'weapons' }
+				))
+				
 }
