b5c1e0034a771c3d59f9d81a30543e4025ee6f1e
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2022-08-18 04:06:53 +0300

    updated reddit command

--- commands/data/reddit.js
+++ commands/data/reddit.js
@@ -6,7 +6,6 @@ const ffmpeg = require('fluent-ffmpeg');
 const request = require("request");
 const fs = require('fs');
 
-
 module.exports = {
     run: async (client, interaction) => {
         let url = interaction.options.getString('url');
@@ -34,58 +33,66 @@ module.exports = {
                 if (data.is_video) {
                     if (!fs.existsSync(`data/reddit`)) fs.mkdirSync('data/reddit');
                     let rnd = Math.ceil(Math.random() * 5000);
-                    let videoClean = data['secure_media']['reddit_video']['fallback_url'];
-                    let video = videoClean.replace("?source=fallback", "").replace("720", "480").replace("1080", "480");
-                    let audio = `${data['url_overridden_by_dest']}/HLS_AUDIO_64_K.aac`;
+                    let video = (data['secure_media']['reddit_video']['fallback_url']).replace("?source=fallback", "").replace("DASH_96", "DASH_720").replace("DASH_1080", "DASH_720");
+                    let audio = `${data['url_overridden_by_dest']}/HLS_AUDIO_64_K`;
                     let videoFile = `data/reddit/output-video-${rnd}.mp4`;
-                    let audioFile = `data/reddit/output-audio-${rnd}.acc`;
+                    let audioFile = `data/reddit/output-audio-${rnd}.aac`;
                     let mergedFile = `data/reddit/output-merged-video-${rnd}.mp4`;
     
-                    await download(audio, audioFile);
                     await download(video, videoFile);
+                    request({
+                        uri: `${audio}.aac`,
+                    }, async function(err, response, body) {
+                        if (response.statusCode === 200) {
+                            await download(`${audio}.aac`, audioFile);
+                        } else {
+                            audioFile = `data/reddit/output-audio-${rnd}.ts`;
+                            await download(`${audio}.ts`, audioFile);
+                        }
 
-                    try {
-                        new ffmpeg(videoFile)
-                            .addInput(audioFile)
-                            .saveToFile(mergedFile).on("end", async () => {
-                                fs.unlinkSync(videoFile);
-                                fs.unlinkSync(audioFile);
-
-                                let stats = fs.statSync(mergedFile);
-                                stats.size = Math.round(stats.size / (1024*1024));
-
-                                if (stats.size > 9) {
-                                    fs.unlinkSync(mergedFile);
-                                    interaction.editReply({ content: await buildText("file_size_large", client, { guild: interaction.guildId }), embeds: [] });
-                                    await sleep(5000);
-                                    return interaction.deleteReply();
-                                }
-
-                                const redditVideo = new AttachmentBuilder()
-                                    .setFile(mergedFile)
-                                    .setName('reddit-video.mp4');
-                                    
-                                const redditButtons = new ActionRowBuilder().addComponents(
-                                    new ButtonBuilder()
-                                        .setStyle(ButtonStyle.Link)
-                                        .setLabel(`Reddit`)
-                                        .setURL(`https://reddit.com${data['permalink']}`),
-                                );
-        
-                                return interaction.editReply({ embeds: [], files: [redditVideo], components: [redditButtons] }).then(() => {
-                                    fs.unlinkSync(mergedFile);
+                        try {
+                            new ffmpeg(videoFile)
+                                .addInput(audioFile)
+                                .saveToFile(mergedFile).on("end", async () => {
+                                    fs.unlinkSync(videoFile);
+                                    fs.unlinkSync(audioFile);
+    
+                                    let stats = fs.statSync(mergedFile);
+                                    stats.size = Math.round(stats.size / (1024*1024));
+    
+                                    if (stats.size > 9) {
+                                        fs.unlinkSync(mergedFile);
+                                        interaction.editReply({ content: await buildText("file_size_large", client, { guild: interaction.guildId }), embeds: [] });
+                                        await sleep(5000);
+                                        return interaction.deleteReply();
+                                    }
+    
+                                    const redditVideo = new AttachmentBuilder()
+                                        .setFile(mergedFile)
+                                        .setName('reddit-video.mp4');
+                                        
+                                    const redditButtons = new ActionRowBuilder().addComponents(
+                                        new ButtonBuilder()
+                                            .setStyle(ButtonStyle.Link)
+                                            .setLabel(`Reddit`)
+                                            .setURL(`https://reddit.com${data['permalink']}`),
+                                    );
+            
+                                    return interaction.editReply({ embeds: [], files: [redditVideo], components: [redditButtons] }).then(() => {
+                                        fs.unlinkSync(mergedFile);
+                                    });
                                 });
-                            });
-                    } catch (error) {
-                        console.log(error);
-
-                        if (fs.existsSync(videoFile)) fs.unlinkSync(videoFile);
-                        if (fs.existsSync(audioFile)) fs.unlinkSync(audioFile);
-                        if (fs.existsSync(mergedFile)) fs.unlinkSync(mergedFile);
-                        interaction.editReply({ content: await buildText("reddit_cannot_upload", client, { guild: interaction.guildId }), embeds: [] });
-                        await sleep(5000);
-                        return interaction.deleteReply();
-                    }
+                        } catch (error) {
+                            console.log(error);
+    
+                            if (fs.existsSync(videoFile)) fs.unlinkSync(videoFile);
+                            if (fs.existsSync(audioFile)) fs.unlinkSync(audioFile);
+                            if (fs.existsSync(mergedFile)) fs.unlinkSync(mergedFile);
+                            interaction.editReply({ content: await buildText("reddit_cannot_upload", client, { guild: interaction.guildId }), embeds: [] });
+                            await sleep(5000);
+                            return interaction.deleteReply();
+                        }
+                    });
                 } else {
                     interaction.editReply({ content: await buildText("reddit_notfound_video", client, { guild: interaction.guildId }), embeds: [] });
                     await sleep(5000);
