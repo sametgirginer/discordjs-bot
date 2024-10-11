f7bb2b4f5b7ff964f85e7da7322a612a07228121
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2024-10-12 02:37:30 +0300

    new instagram

--- commands/data/instagram.js
+++ commands/data/instagram.js
@@ -2,12 +2,13 @@ const { SlashCommandBuilder, AttachmentBuilder, ActionRowBuilder, ButtonBuilder,
 const { buildText } = require("../../functions/language");
 const { download } = require("../../functions/download");
 const { sleep, rotateMedia } = require("../../functions/helpers");
-const request = require("request");
+const puppeteer = require('puppeteer');
+//const request = require("request");
 const fs = require("fs");
 
 module.exports = {
     run: async (client, interaction) => {
-        if (process.env.instagramcookies.length <= 0) return interaction.reply({ content: await buildText("command_inactive", client, { guild: interaction.guildId }), ephemeral: true });
+        //if (process.env.instagramcookies.length <= 0) return interaction.reply({ content: await buildText("command_inactive", client, { guild: interaction.guildId }), ephemeral: true });
         
         let url = interaction.options.getString("url");
         let regex = /((?:https?:\/\/)?(?:www\.)?instagram\.com\/(?:p|reel|reels)\/([^/?#&]+)).*/;
@@ -15,6 +16,87 @@ module.exports = {
 
         if (match) {
             interaction.deferReply().then(async () => {
+                const xhr_result = async (result) => {
+                    if (result.data.xdt_shortcode_media) {
+                        let data = result.data.xdt_shortcode_media;
+
+                        if (data.edge_sidecar_to_children != undefined) {
+                            data.edge_sidecar_to_children.edges.forEach(n => {
+                                if (n.node.is_video) {
+                                    data.is_video = true;
+                                    data.video_url = n.node.video_url;
+                                }
+                            });
+                        }
+
+                        if (data.is_video) {
+                            if (!fs.existsSync(`data/instagram`)) fs.mkdirSync('data/instagram');
+                            let rnd = Math.ceil(Math.random() * 5000);
+                            let videoFile = `data/instagram/insta-video-${rnd}.mp4`;
+                    
+                            try {
+                                await download(data.video_url, videoFile);
+                                if (interaction.options.getBoolean('rotate')) videoFile = await rotateMedia(videoFile);
+                    
+                                let stats = fs.statSync(videoFile);
+                                stats.size = Math.round(stats.size / (1024*1024));
+                    
+                                if (stats.size > 9) {
+                                    fs.unlinkSync(videoFile);
+                                    return interaction.editReply({ content: await buildText("file_size_large", client, { guild: interaction.guildId }), ephemeral: true }).then(async () => {
+                                        await sleep(5000);
+                                        interaction.deleteReply();
+                                    });
+                                }
+                    
+                                const instaVideo = new AttachmentBuilder()
+                                    .setFile(videoFile)
+                                    .setName('instagram-video.mp4');
+                                    
+                                const instaButton = new ActionRowBuilder().addComponents(
+                                    new ButtonBuilder()
+                                        .setStyle(ButtonStyle.Link)
+                                        .setLabel(await buildText("button_view_onsite", client, { guild: interaction.guildId }))
+                                        .setURL(url)
+                                );
+                            
+                                return interaction.editReply({ files: [instaVideo], components: [instaButton] }).then(async () => {
+                                    fs.unlinkSync(videoFile);
+                                });
+                            } catch (error) {
+                                console.log(error);
+                    
+                                if (fs.existsSync(videoFile)) fs.unlinkSync(videoFile);
+                                interaction.editReply({ content: await buildText("error_downloading_data", client, { guild: interaction.guildId }), ephemeral: true }).then(async () => {
+                                    await sleep(5000);
+                                    interaction.deleteReply();
+                                });
+                            }
+                        } else {
+                            return interaction.editReply({ content: await buildText("notfound_video", client, { guild: interaction.guildId }), ephemeral: true });
+                        }
+                    } else {
+                        return interaction.editReply({ content: await buildText("notfound_media", client, { guild: interaction.guildId }), ephemeral: true });
+                    }
+                }
+                
+                (async () => {
+                    const browser = await puppeteer.launch({ headless: false });
+                    const page = await browser.newPage();
+                    await page.goto(`https://www.instagram.com/p/${match[2]}/`);
+
+                    page.on('response', async (response) => {
+                        const url = response.url();
+                    
+                        if (url.includes('query')) {
+                            json = await response.json();
+                            xhr_result(json);
+                            await browser.close();
+                        }
+                    });
+                })();
+
+                /*
                 request({
                     uri: `https://www.instagram.com/p/${match[2]}/?__a=1&__d=dis`,
                     json: true,
@@ -146,7 +228,7 @@ module.exports = {
                     } else {
                         return interaction.editReply({ content: await buildText("notfound_media", client, { guild: interaction.guildId }), ephemeral: true });
                     }
-                });
+                });*/
             });
         } else {
             return interaction.reply({ content: await buildText("unvaild_url", client, { guild: interaction.guildId, variables: ["https://www.instagram.com/p/XXXXXXX/"] }), ephemeral: true });
